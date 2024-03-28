58a68f868bfc19b7e61415c85dbeab4610e62b92
Magnus Zetterberg <magnus.zetterberg@gmail.com>
2024-03-28 13:56:09 +0100

    small fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "** Setting up packages **"
+echo ""
 sudo apt install python3-pip && pip install -r req.txt
-
+echo ""
 echo "*** Download networks for Object Detections ***"
 ~/.local/bin/omz_downloader --name person-detection-0200 && echo "" && echo "*** OpenVino Networks are downloaded ***"
 
